fba102fc20dff6664ba71bd7a69a789cf6879afb
Gustavo Cardoso <gustavocardoso@gmail.com>
2017-11-24 15:50:11 -0200

    Add component files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <img src="./assets/logo.png" class="logo">
+    <h1>Vue Github Profile</h1>
+    <search-profile></search-profile>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import searchProfile from './components/SearchProfile.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+
+    components: {
+      searchProfile
+    },
+
+    data () {
+      return {
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font: normal normal 16px/1.5 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    overflow-x: hidden;
+  }
+
+  #app {
+    width: 90%;
+    text-align: center;
+    color: #2c3e50;
+    margin: 0 auto;
+    padding: 2em 2em 0 2em;
+  }
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .logo {
+    width: 80px;
+  }
 </style>
---
+++ src/components/SearchProfile.vue
@@ -0,0 +1,232 @@
+<template>
+  <div>
+    <div class="search-box">
+      <input type="text" v-model="profile" placeholder="type a github profile" v-on:focus="clearField" class="profile">
+      <button v-on:click="fetchProfile" class="search">search</button>
+    </div>
+
+    <div class="loader" v-if="show.loader">
+      <div class="bullet"></div>
+      <div class="bullet"></div>
+      <div class="bullet"></div>
+    </div>
+
+    <div v-if="show.user" class="user-profile">
+      <img v-bind:src="user.avatar_url" class="avatar">
+
+      <div class="info">
+        <h2 class="name">{{ user.name }}</h2>
+        <p class="login">{{ user.login }}</p>
+
+        <ul class="meta">
+          <li class="repos">Repos: <strong>{{ user.public_repos }}</strong></li>
+          <li class="followers">Followers: <strong>{{ user.followers }}</strong></li>
+          <li class="following">Following: <strong>{{ user.following }}</strong></li>
+        </ul>
+      </div>
+    </div>
+
+    <p class="error-message" v-if="show.error">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchProfile',
+
+    data () {
+      return {
+        apiUrl: 'https://api.github.com/users',
+        error: null,
+        profile: '',
+        show: {
+          error: false,
+          loader: false,
+          user: false,
+        },
+        user: {
+          avatar_url: null,
+          followers: null,
+          following: null,
+          location: null,
+          login: null,
+          name: null,
+          public_repos: null
+        },
+
+      }
+    },
+
+    methods: {
+      clearField() {
+        this.profile = ''
+      },
+
+      fetchProfile() {
+        if (!this.user.login || this.user.login != this.profile) {
+          this.error = null
+          this.show.error = false
+          this.show.user = false
+          this.show.loader = true
+
+          setTimeout(() => {
+            fetch(`${this.apiUrl}/${this.profile}`)
+            .then(response => {
+              if (response.ok) {
+                return response.json()
+              } else {
+                throw new Error('User not found')
+              }
+            })
+            .then(data => {
+              this.user.avatar_url = data.avatar_url
+              this.user.followers = data.followers
+              this.user.following = data.following
+              this.user.location = data.location
+              this.user.login = data.login
+              this.user.name = data.name
+              this.user.public_repos = data.public_repos
+              this.show.loader = false
+              this.show.user = true
+            })
+            .catch(err => {
+              this.error = err.message
+              this.show.error = true
+            })
+          }, 2000)
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    margin: 0;
+  }
+
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .profile {
+    display: block;
+    width: 50%;
+    font-size: 1em;
+    font-weight: 700;
+    text-align: center;
+    border-radius: .4em;
+    border: 1px solid #ddd;
+    margin-bottom: 1em;
+    padding: .4em;
+    color: #35495E;
+  }
+
+  .search {
+    display: block;
+    font-size: 1em;
+    font-weight: 700;
+    color: #eee;
+    background: #41B883;
+    border-radius: .4em;
+    border: 0;
+    padding: .6em;
+  }
+
+  .user-profile {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 3em 0;
+  }
+
+  .avatar {
+    width: 40%;
+    border-radius: 2em;
+    border: 5px solid #eee;
+    box-shadow: 0 .2em 1em 0 rgba(0, 0, 0, 0.2);
+    margin-bottom: 1em;
+  }
+
+  .name {
+    font-size: 2em;
+    font-weight: 700;
+    letter-spacing: -.05em;
+    color: #35495E;
+    margin: 0;
+  }
+
+  .login {
+    font-size: 1.4em;
+    color: #41B883;
+  }
+
+  .loader {
+    display: flex;
+    width: 50%;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: 3em auto 0 auto;
+  }
+
+  .bullet {
+    width: 1em;
+    height: 1em;
+    background: #ccc;
+    border-radius: 50%;
+    margin: 0 1em;
+  }
+
+  .bullet:nth-child(1) {
+    animation: bounce .5s infinite ease-in-out;
+  }
+  .bullet:nth-child(2) {
+    animation: bounce .5s .1s infinite ease-in-out;
+  }
+  .bullet:nth-child(3) {
+    animation: bounce .5s .2s infinite ease-in-out;
+  }
+
+  @keyframes bounce {
+    0% {
+      transform: scale3d(1, 1, 1);
+      opacity: 1;
+    }
+    50% {
+      transform: scale3d(1.4, 1.4, 1.4);
+      opacity: .1;
+    }
+    100% {
+      transform: scale3d(1, 1, 1);
+      opacity: 1;
+    }
+  }
+
+  @media screen and (min-width: 50em) {
+    .profile {
+      width: 22%;
+      padding: .6em;
+    }
+
+    .user-profile {
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      margin: 3em 0;
+    }
+
+    .avatar {
+      width: 20%;
+    }
+
+    .info {
+      text-align: left;
+      padding: 0 2em;
+    }
+  }
+</style>
